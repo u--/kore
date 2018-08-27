d5870a7473dd31cddd3eb85d779a1f9ddd27fe93
Phillip Harris <palindromeh@gmail.com>
2018-08-27 11:59:30 -0500

    pedantic

--- src/main/haskell/kore/test/Test/Kore/Builtin/Map.hs
+++ src/main/haskell/kore/test/Test/Kore/Builtin/Map.hs
@@ -1,12 +1,10 @@
 module Test.Kore.Builtin.Map where
 
-import Test.QuickCheck
-       ( Property, (===) )
+-- import Test.QuickCheck
+--        ( Property, (===) )
 
 import qualified Data.Map
 -- import qualified Data.Map as Map
-import           Data.Proxy
-                 ( Proxy (..) )
 
 import           Kore.AST.Common
 import           Kore.AST.MetaOrObject
@@ -15,20 +13,20 @@ import           Kore.AST.PureML
                  ( CommonPurePattern )
 import           Kore.AST.Sentence
 import           Kore.ASTUtils.SmartPatterns
-import           Kore.ASTUtils.SmartConstructors
-import           Kore.ASTVerifier.DefinitionVerifier
-import           Kore.ASTVerifier.Error
-                 ( VerifyError )
+-- import           Kore.ASTUtils.SmartConstructors
+-- import           Kore.ASTVerifier.DefinitionVerifier
+-- import           Kore.ASTVerifier.Error
+                 -- ( VerifyError )
 import qualified Kore.Builtin as Builtin
 import qualified Kore.Builtin.Map as Map
-import           Kore.Builtin.Hook
-                 ( hookAttribute )
-import qualified Kore.Error
+-- import           Kore.Builtin.Hook
+                 -- ( hookAttribute )
+-- import qualified Kore.Error
 import           Kore.IndexedModule.IndexedModule
-import           Kore.IndexedModule.MetadataTools
-import           Kore.Step.ExpandedPattern
-import           Kore.Step.Simplification.Data
-import qualified Kore.Step.Simplification.Pattern as Pattern
+-- import           Kore.IndexedModule.MetadataTools
+-- import           Kore.Step.ExpandedPattern
+-- import           Kore.Step.Simplification.Data
+-- import qualified Kore.Step.Simplification.Pattern as Pattern
 import           Kore.Step.StepperAttributes
                  ( StepperAttributes )
 import           Test.Kore.Builtin.Builtin
@@ -36,8 +34,8 @@ import           Test.Kore.Builtin.Builtin
 import Test.Kore
        ( testId )
 
-import Debug.Trace
-import Kore.Proof.Dummy
+-- import Debug.Trace
+-- import Kore.Proof.Dummy
 
 -- | A sort to hook to the builtin @MAP.Map@.
 -- Should actually be parametric
@@ -108,8 +106,8 @@ lookupFromElem
     -> CommonPurePattern Object
 lookupFromElem k1 k2 v = App_ symLookup [k1, App_ symElement [k2, v]]
 
-tryLookupFromElem k1 k2 v = evaluate indexedModule (lookupFromElem k1 k2 v)
+-- tryLookupFromElem k1 k2 v = evaluate indexedModule (lookupFromElem k1 k2 v)
 
-pat1 = dummyEnvironment $ mkVar (var_ "KEY" "*") :: CommonPurePattern Object
-pat2 = dummyEnvironment $ mkVar (var_ "VALUE" "*") :: CommonPurePattern Object
+-- pat1 = dummyEnvironment $ mkVar (var_ "KEY" "*") :: CommonPurePattern Object
+-- pat2 = dummyEnvironment $ mkVar (var_ "VALUE" "*") :: CommonPurePattern Object
 
